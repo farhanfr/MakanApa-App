17a6a7f9fe7b7b3c44203460cc76373ba2651789
farhanfitrahtur <farhanfitrahtur@gmail.com>
2020-06-19 21:57:45 +0700

    setup project & add splash screen

---
+++ lib/page/splash_screen/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:makan_apa_app/page/authentication/login_screen.dart';
+import 'package:makan_apa_app/widget/constanst.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/img/splashscreen.png",
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              "Makan Apa",
+              // textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 32.0,
+                  height: 1.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Poppins"),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            if (isLoading)
+              SizedBox(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue[200]),
+                ),
+              )
+            else
+              Text("1")
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/constanst.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2C9DF0);
+const secondaryColor = Color(0xFFB5BFD0);
+const textColor = Color(0xFF50505D);
+const textLightColor = Color(0xFF6A727D);
