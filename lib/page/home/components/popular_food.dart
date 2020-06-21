171af84e1c5d5c41ebc95dcf70ad119e04ddfc45
farhanfitrahtur <farhanfitrahtur@gmail.com>
2020-06-21 14:35:40 +0700

    add popular food

---
+++ lib/page/home/components/popular_food.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:makan_apa_app/widget/constanst.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+class PopularFood extends StatelessWidget {
+  const PopularFood({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      // padding: EdgeInsets.only(bottom: 10),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 2,
+        itemBuilder: (context, int index) {
+          return Container(
+            margin: EdgeInsets.only(left: 20, right: 10, top: 20, bottom: 40),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 4),
+                    blurRadius: 20,
+                    color: Color(0xFFB0CCE1).withOpacity(0.32),
+                  ),
+                ]),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                // onTap: press,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: Image.asset("assets/img/bgtest.png",
+                          fit: BoxFit.cover),
+                    ),
+                    Container(
+                      // margin: EdgeInsets.only(right: 10),
+                      padding: EdgeInsets.only(left: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 80.0),
+                            child: Text(
+                              "Mie Setan2",
+                              style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0),
+                            ),
+                          ),
+                           SmoothStarRating(
+                            size: 20.0,
+                            borderColor: Colors.yellow,
+                            color: Colors.yellow,
+                            rating: 3.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                
+                    
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/page/home/home_screen.dart
+++ lib/page/home/home_screen.dart
@@ -4,33 +4,71 @@ import 'package:flutter/material.dart';
 import 'package:makan_apa_app/page/home/components/carousel_info.dart';
 import 'package:makan_apa_app/page/home/components/food_category_one.dart';
 import 'package:makan_apa_app/page/home/components/food_category_two.dart';
+import 'package:makan_apa_app/page/home/components/popular_food.dart';
 import 'package:makan_apa_app/widget/constanst.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteColor,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-        child: Column(
-          children: <Widget>[
-            CarouselInfo(),
-            SizedBox(height: 20.0),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Food Category",style: TextStyle(fontFamily:"Poppins",fontWeight: FontWeight.bold,fontSize: 20.0,color: primaryColor)),
-            ),
-            SizedBox(height: 20.0),
-            FoodCategoryOne(),
-            SizedBox(height: 25.0,),
-            FoodCategoryTwo()
-            
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          // padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: <Widget>[
+                    CarouselInfo(),
+                    SizedBox(height: 20.0),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Food Category",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              color: primaryColor)),
+                    ),
+                    SizedBox(height: 20.0),
+                    FoodCategoryOne(),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    FoodCategoryTwo(),
+                    SizedBox(height: 25.0),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Popular Food",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              color: primaryColor)),
+                    ),
+                  ],
+                ),
+              ),
+              PopularFood(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child : Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("New Food This Week",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              color: primaryColor)),
+                    ),
+              )
+              
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
